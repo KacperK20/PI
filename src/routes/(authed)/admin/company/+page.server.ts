1d85b02ddeda203a6cafc22281f3587ab51c28cb
Kacper <130649859+KacperK20@users.noreply.github.com>
2024-03-26 17:27:56 +0100

    Delete src/routes/(authed)/admin/company directory

--- src/routes/(authed)/admin/company/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import type { PageServerLoad } from './$types';
-import type { Action, Actions, PageServerLoad } from './$types'
-import { fail, redirect } from '@sveltejs/kit'
-
-export const load = (async () => {
-    return {};
-}) satisfies PageServerLoad;
